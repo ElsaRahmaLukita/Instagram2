973bb68b1cd6fac51435c2d5d58b0cc42c528b91
ElsaRahmaLukita <31659621+ElsaRahmaLukita@users.noreply.github.com>
2017-10-16 08:06:43 +0700

    Tugas Form Instagram
    Nama : Elsa Rahma Lukita
    Kelas : XI RPL 2
    No : 12
    Tugas Form Instagram

---
+++ Instagram/login1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Template Login | 1</title>
+    <link rel="stylesheet" href="style-1.css">
+    <meta name="viewport" content="width=device-width , initial-scale=1">
+  </head>
+  <body>
+
+    <div id="login">
+      <div class="center">
+
+          <h2><font size="5">Login With Facebook</font></h2>
+
+          <form class="fl" action="" method="post">
+            <input class="itpw" type="text" name="username" placeholder="Username or Email"><br>
+            <input class="itpw" type="password" name="password" placeholder="Password"><br>
+            <input class="its" type="submit" name="login" value="LOGIN">
+
+          </form>
+
+          <p><a href="#">Forget your password ?</a> | <a href="#">Created an account</a>  </p>
+
+      </div>
+    </div>
+
+  </body>
+</html>
